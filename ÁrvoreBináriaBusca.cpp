8ca356ca0ba06ad0a4d50508d2d88d4d4f4c4046
GabrielFaria1509 <fariagabriel1509@gmail.com>
2025-05-08 14:05:57 -0300

    Update ÁrvoreBináriaBusca.cpp

--- ÁrvoreBináriaBusca.cpp
+++ ÁrvoreBináriaBusca.cpp
@@ -2,68 +2,77 @@
 using namespace std;
 
 struct node {
-    int info;
-    node* left;
-    node* right;
+	int info;
+	node * left;
+	node * right;
 };
 
 void insert(node * &curr, int info){
-    if(curr == NULL){
-        curr = new node;
-        curr->info = info;
-        curr->left = NULL;
-        curr->right = NULL;
-    }else{
-        if(info < curr->info){
-            insert(curr->left, info); // Chama a função recursivamente para o filho esquerdo
-        }else{
-            insert(curr->right, info); // Chama a função recursivamente para o filho direito
-        }
-    }
+	if(curr == NULL){
+		curr = new node;
+		curr->info = info;
+		curr->left = NULL;
+		curr->right = NULL;
+	} else {
+		if(info < curr->info){
+			insert(curr->left, info);
+		} else {
+			insert(curr->right, info);
+		}
+	}
 }
 
-node * search(node * curr, int info){
-    if(curr == NULL){
-        return NULL; // Se o nó atual for nulo, retorna nulo
-    }
-    else{
-        if(curr->info == info){
-            return curr; // Se o nó atual contém o valor procurado, retorna o nó
-        }else{
-            if(info < curr->info){
-                return search(curr->left, info); // Chama a função recursivamente para o filho esquerdo
-            }
-            else{
-                return search(curr->right, info); // Chama a função recursivamente para o filho direito
-            }
-        }
-    }
+node * search (node * curr, int info){
+	if(curr == NULL){
+		return NULL;
+	} else {
+		if(curr->info == info){
+			return curr;
+		} else {
+			if(info < curr->info){
+				return search(curr->left, info);
+			} else {
+				return search(curr->right, info);
+			}
+		}
+	}
+	
 }
 
 int main(){
-    node * root = NULL; // Inicializa a raiz da árvore como nula
-
-    insert(root, 10); // Insere o valor 10 na árvore
-
-    insert(root, 5); // Insere o valor 5 na árvore
-
-    insert(root, 15); // Insere o valor 15 na árvore
-
-    node * result = search(root,11); // Busca o valor 11 na árvore
-
-    if(result == NULL){
-        cout << "Valor não encontrado na árvore." << endl; // Se o valor não for encontrado, imprime mensagem
-    }else{
-        cout << result->info << endl;
-    }
-
-    result = search(root,6);
-    
-    if(result == NULL){
-        cout << 6 << " não encontrado na árvore." << endl; // Se o valor não for encontrado, imprime mensagem
-    }else{
-        cout << result->info << endl;
-    }
-
-    return 0; // Retorna 0 para indicar que o programa foi executado com sucesso
+	
+	node * root = NULL;
+	
+	insert(root, 10);
+	
+	insert(root, 12);
+	
+	insert(root, 6);
+	
+	insert(root, 33);
+	
+	node * result = search(root, 11);
+	
+	if(result == NULL) {
+		cout << 11 << " nao encontrado" << endl;
+	} else {
+		cout << result->info << endl;
+	}
+	
+	result = search(root, 6);
+	
+	if(result == NULL) {
+		cout << 6 << " nao encontrado" << endl;
+	} else {
+		cout << result->info << endl;
+	}
+	
+	return 0;
 }
+
+
+
+
+
+
+
